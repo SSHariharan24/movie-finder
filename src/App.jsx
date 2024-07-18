4de75f38cd2587541346d4cf1b00dff52cf2ee5d
SSHariharan24 <hariabi2004a@gmail.com>
2024-07-19 00:11:06 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=d2b5c9e";
+const API_URL = "https://www.omdbapi.com?apikey=d2b5c9e";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
